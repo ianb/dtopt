e9807649d8963de8a040c2be600595d8e93e8cff
ianb <ianb@localhost>
2007-08-10 18:22:12 +0000

    some docs/setup.py setup

--- setup.py
+++ setup.py
@@ -3,24 +3,23 @@ import sys, os
 
 version = '0.1'
 
+index_txt = os.path.join(os.path.dirname(__file__), 'docs', 'index.txt')
+long_desc = '\n'.join(open(index_txt).readlines()[2:])
+
 setup(name='dtopt',
       version=version,
       description="Add options to doctest examples while they are running",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description=long_desc,
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Testing",
+      ],
       keywords='doctest',
       author='Ian Bicking',
       author_email='ianb@colorstudy.com',
-      url='',
+      url='http://pypi.python.org/pypi/dtopt/',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      zip_safe=True,
       )
